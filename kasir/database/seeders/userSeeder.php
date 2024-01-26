708fd68be10861173ec4ad5f9a17942a81802a2e
dedypurwodarminto <dedy.purwodarminto@gmail.com>
2024-01-26 09:26:40 +0800

    Membuat user Seeder dan user Model

--- kasir/database/seeders/userSeeder.php
+++ kasir/database/seeders/userSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class userSeeder extends Seeder
 {
@@ -12,6 +15,11 @@ class userSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('tb_users')->insert([
+            'name' => 'admin',
+            'email' => 'dedy.purowdarminto@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin'
+        ]);
     }
 }
