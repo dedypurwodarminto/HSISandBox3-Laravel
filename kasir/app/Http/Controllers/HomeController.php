b02915099d0de8c66880f7c751015b18f58b8118
dedypurwodarminto <dedy.purwodarminto@gmail.com>
2024-01-26 18:30:54 +0800

    Membuat halaman login dan home (controller dan routing)

---
+++ kasir/app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $pages = array(
+            'title' => 'Home Page'
+        );
+
+        return view('index', $pages);
+        // return view('home', $pages);
+    }
+}
